ba3f703b28c9f609d565ac3a2bb112f90ec2b59f
Rahul Prajapati <rahul.prajapati1127@gmail.com>
2025-02-27 23:40:55 +0530

    feat : first VS commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const express = require('express')
+const cors = require("cors");
+const { authRoutes } = require("./routes/auth");
+const app = express()
+const bodyParser = require("body-parser");
+const errorHandler = require("./middleware/errorHandler");
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(errorHandler)
+app.use('/api/auth', authRoutes);
+
+PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to MongoDB!');
+    app.listen(PORT, (res) =>{
+        console.log('app is running on port 5000');
+       })
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res, next) => {
+    try {
+        res.send("Project is initiated");
+    } catch (err) {
+        next(err);
+    }
+});
+
---
+++ routes/auth.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+router.post("/signup", async (req, res, next) => {
+    try {
+        console.log("Backend is started");
+        const { firstname, lastname, email, password } = req.body;
+        console.log(req.body);
+        console.log(firstname, lastname, email, password);
+
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: "Email already exists" });
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const user = new userModel({
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword,
+        });
+        await user.save();
+
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, { expiresIn: "1h" });
+
+        res.status(201).json({ message: "Signup successful", token, user: user });
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+router.post("/signin", async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = await userModel.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid Credentials" });
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(401).json({ message: "Invalid Credentials" });
+        }
+        const payload = {
+            id: user._id,
+            firstname: user.firstname,
+        };
+        const token = jwt.sign(payload, process.env.SECRET_KEY);
+        res.json({ message: "Login successful", token, user }).status(200);
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+module.exports = { authRoutes: router };
